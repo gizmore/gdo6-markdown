fd5cf9419ba40f28dded419c3579d02774b9ef3d
gizmore <gizmore@wechall.net>
2021-10-31 01:39:02 +0200

    6.10.6-r6146_dog_is_rising

--- js/gdo6-markdown.js
+++ js/gdo6-markdown.js
@@ -10,6 +10,15 @@ $(function() {
 			path: GDO_WEB_ROOT + 'GDO/Markdown/bower_components/editor.md/lib/',
 			placeholder: '',
 			autoFocus: false,
+			toolbarIcons: [
+	            "undo", "redo", "|", 
+    	        "bold", "italic", "quote", "|", 
+	            "h1", "h2", "h3", "h4", "|", 
+            	"hr", "|",
+            	"link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "emoji", "html-entities", "pagebreak", "|",
+            	"watch", "preview", "fullscreen", "clear",
+			],
+			autoHeight: true,
 		});
 	}); 
 });
