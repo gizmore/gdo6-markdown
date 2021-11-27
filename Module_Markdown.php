ec9d8a2a8bf5da2e819fe3b4baa2ca1f4960d02c
gizmore <gizmore@wechall.net>
2021-11-27 20:28:38 +0100

    6.11.0-r6200

--- Module_Markdown.php
+++ Module_Markdown.php
@@ -14,93 +14,96 @@ use GDO\Language\Trans;
  * Uses markdown from 
  * 
  * @author gizmore
- * @version 6.10.6
- * @since 6.10.1
+ * @version 6.11.0
+ * @since 6.10.5
  */
 final class Module_Markdown extends GDO_Module
 {
-    public function getDependencies()
-    {
-        return [
-            'JQuery',
-            'FontAwesome',
-        ];
-    }
-    
-    public function getModuleLicenseFilenames()
-    {
-        return [
-            'markdown/LICENSE',
-            'bower_components/editor.md/LICENSE',
-            'bower_components/editor.md/lib/codemirror/LICENSE',
-            'LICENSE',
-        ];
-    }
-    
-    public function getConfig()
-    {
-        return [
-            GDT_Checkbox::make('markdown_decoder')->initial('1'),
-            GDT_Checkbox::make('markdown_js_editor')->initial('1'),
-        ];
-    }
-    public function cfgDecoder() { return $this->getConfigVar('markdown_decoder'); }
-    public function cfgJSEditor() { return $this->getConfigVar('markdown_js_editor'); }
-    
-    public function onInit()
-    {
-        if ($this->cfgDecoder())
-        {
-            GDT_Message::$DECODERS['Markdown'] =
-            GDT_Message::$DECODER = [self::class, 'decode'];
-            GDT_Message::$EDITOR_NAME = 'Markdown';
-            spl_autoload_register([$this, 'autoloadMarkdown']);
-        }
-    }
+	public function getDependencies()
+	{
+		return [
+			'JQuery',
+			'FontAwesome',
+		];
+	}
+	
+	public function getModuleLicenseFilenames()
+	{
+		return [
+			'markdown/LICENSE',
+			'bower_components/editor.md/LICENSE',
+			'bower_components/editor.md/lib/codemirror/LICENSE',
+			'LICENSE',
+		];
+	}
+	
+	public function getConfig()
+	{
+		return [
+			GDT_Checkbox::make('markdown_decoder')->initial('1'),
+			GDT_Checkbox::make('markdown_js_editor')->initial('1'),
+		];
+	}
+	public function cfgDecoder() { return $this->getConfigVar('markdown_decoder'); }
+	public function cfgJSEditor() { return $this->getConfigVar('markdown_js_editor'); }
+	
+	public function onInit()
+	{
+		if ($this->cfgDecoder())
+		{
+			GDT_Message::$DECODERS['Markdown'] =
+			GDT_Message::$DECODER = [self::class, 'decode'];
+			GDT_Message::$EDITOR_NAME = 'Markdown';
+			spl_autoload_register([$this, 'autoloadMarkdown']);
+		}
+	}
 
-    public static function decode($input)
-    {
-        $html = (new Decoder($input))->decoded();
-        return GDT_Message::getPurifier()->purify($html);
-    }
-    
-    public function autoloadMarkdown($class)
-    {
-        if (Strings::startsWith($class, 'cebe\\markdown\\'))
-        {
-            $class = Strings::substrFrom($class, 'cebe\\markdown\\');
-            $class = str_replace('\\', '/', $class) . '.php';
-            $class = $this->filePath('markdown/'.$class);
-            require_once $class;
-        }
-    }
-    
-    public function onIncludeScripts()
-    {
-        if ($this->cfgJSEditor())
-        {
-            $min = Module_Javascript::instance()->jsMinAppend();
-            $this->addBowerJS("editor.md/editormd{$min}.js");
-            $this->addJS('js/gdo6-markdown.js');
-        }
-        $this->addBowerCSS("editor.md/css/editormd{$min}.css");
-        $this->addCSS('css/gdo6-markdown.css');
-        
-        # Load language pack
-        switch (Trans::$ISO)
-        {
-            case 'de':
-                $this->addJS('js/editor.md_de.js');
-                break;
-            default:
-                $this->addBowerJS("editor.md/languages/en.js");
-                break;
-        }
-    }
-    
-    public function hookIgnoreDocsFiles(GDT_Array $ignore)
-    {
-        $ignore->data[] = 'GDO/Markdown/markdown/**/*';
-    }
-    
+	public static function decode($input)
+	{
+		$html = (new Decoder($input))->decoded();
+		return GDT_Message::getPurifier()->purify($html);
+	}
+	
+	public function autoloadMarkdown($class)
+	{
+		if (Strings::startsWith($class, 'cebe\\markdown\\'))
+		{
+			$class = Strings::substrFrom($class, 'cebe\\markdown\\');
+			$class = str_replace('\\', '/', $class) . '.php';
+			$class = $this->filePath('markdown/'.$class);
+			require_once $class;
+		}
+	}
+	
+	public function onIncludeScripts()
+	{
+		$min = Module_Javascript::instance()->jsMinAppend();
+		if ($this->cfgJSEditor())
+		{
+			$this->addBowerJS("editor.md/editormd{$min}.js");
+			$this->addJS('js/gdo6-markdown.js');
+			$this->addCSS("css/editormd.css");
+		}
+		$this->addCSS('css/gdo6-markdown.css');
+		
+		# Load language pack
+		if ($this->cfgJSEditor())
+		{
+			switch (Trans::$ISO)
+			{
+				case 'de':
+					$this->addJS('js/editor.md_de.js');
+					break;
+				default:
+					$this->addBowerJS("editor.md/languages/en.js");
+					break;
+			}
+		}
+	}
+	
+	public function hookIgnoreDocsFiles(GDT_Array $ignore)
+	{
+		$ignore->data[] = 'GDO/Markdown/markdown/**/*';
+	}
+	
 }
